608f1cb1a09dc8d5957b139a4ca78ad8f2282945
Ayan Ghosh <95524790+Ayan726@users.noreply.github.com>
2023-06-08 23:37:50 +0530

    Create Two-sum.py

---
+++ Arrays-part-4/Two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = defaultdict(int)
+
+        for i in range(len(nums)):
+            if target-nums[i] in dic:
+                return [i, dic[target-nums[i]]]
+            dic[nums[i]] = i
+        
+        return []
