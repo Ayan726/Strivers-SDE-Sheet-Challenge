f58f610fe97eb069fb188b2685be85b1c6d6ea20
Ayan Ghosh <95524790+Ayan726@users.noreply.github.com>
2023-06-15 15:05:22 +0530

    Create Maximum-activities.py

---
+++ Greedy-algo/Maximum-activities.py
@@ -0,0 +1,15 @@
+def maximumActivities(start, end):
+    n = len(start)
+    l = []
+    for i in range(n):
+        l.append([start[i], end[i]])
+    data = sorted(l, key=lambda x: x[1])
+    maxi = data[0][1]
+    res, i = 1, 1
+    
+    while i<n:
+        if data[i][0] >= maxi:
+            res += 1
+            maxi = data[i][1]
+        i += 1
+    return res
