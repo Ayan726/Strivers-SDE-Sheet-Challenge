a63234b176e0706f2cdb3fc412483b56bbde22bf
Ayan Ghosh <95524790+Ayan726@users.noreply.github.com>
2023-06-26 12:39:32 +0530

    Create Clone-a-graph.cpp

---
+++ Graph-part-1/Clone-a-graph.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+    unordered_map<Node*, Node*> m;
+    void dfs(Node* node, Node* parent){
+        if(m.count(node)){
+            if(parent->val != -1)parent->neighbors.push_back(m[node]);
+            return;
+        }
+        Node* neww = new Node(node->val);
+        if(parent->val != -1)parent->neighbors.push_back(neww);
+        m[node] = neww;
+
+        for(auto it: node->neighbors){
+            dfs(it, neww);
+        }
+    }
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node)return NULL;
+        dfs(node, new Node(-1));
+        return m[node];
+    }
+};
